5556fefc30cfde8f9488fff5a50e84a12a50b88d
Rogers Ruga <rogersruga.g@gmail.com>
2025-11-08 07:54:44 +0300

    A script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,11 @@
+function multiLanguages(){
+    let firstLine = "C is fun";
+    let secondLine = "Python is cool";
+    let thirdLine = "Javascript is amazing";
+
+    console.log(firstLine);
+    console.log(secondLine);
+    console.log(thirdLine);
+}
+
+multiLanguages();
